e21d7a7fa4955862079743334d10b7ca429e0044
Salvador Planas <101177251+splanaspe@users.noreply.github.com>
2024-02-18 01:01:42 +0100

    primer esbozo de la tienda

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+    <Header> </Header>
+    <div class="grid grid-cols-[15%_auto] text-start px-4 my-10 gap-4 relative">
+        <div class="bg-gray-100 rounded-md self-start sticky top-4 border-2 border-purple-800 shadow-sm">
+            <ul class="py-4 px-5 text-center">
+                <li class="my-1 cursor-pointer text-lg text-purple-800 hover:text-purple-600 ">  Jilbabs </li>
+                <li class="my-1 cursor-pointer text-lg text-purple-800 hover:text-purple-600 "> Hijabs  </li>
+                <li class="my-1 cursor-pointer text-lg text-purple-800 hover:text-purple-600 "> Abayas  </li>
+                <li class="my-1 cursor-pointer text-lg text-purple-800 hover:text-purple-600 "> Bolsos  </li>
+                <li class="my-1 cursor-pointer text-lg text-purple-800 hover:text-purple-600 ">  Accesorios </li>
+            </ul>
+        </div>
+
+        <div class="grid grid-cols-3 gap-4 content-start snap-y ">
+            <div class="border rounded-md p-3 basis-1/4 shadow-sm text-center"> 
+                
+                <img class="rounded-md" src="./assets/jilbab1.webp">
+                <h1 class="font-bold"> Jilbab Azul Cielo </h1><p> 30 $</p>
+            </div>
+            <div class="border rounded-md p-3 basis-1/4 shadow-sm  text-center"> 
+                
+                <img class="rounded-md" src="./assets/jilbab2.webp">
+                <h1> Nombre Producto </h1><p> 40 $</p>
+            </div>
+            <div class="border rounded-md p-3 basis-1/4 shadow-sm  text-center"> 
+                
+                <img class="rounded-md" src="./assets/jilbab3.webp">
+                <h1> Nombre Producto </h1>
+                <p> 40 $</p>
+            </div>
+            <div class="border rounded-md p-3 basis-1/4 shadow-sm  text-center"> 
+                
+                <img class="rounded-md" src="./assets/jilbab4.webp">
+                <h1> Nombre Producto </h1>
+                <p> 40 $</p>
+            </div>
+            <div class="border rounded-md p-3 basis-1/4 shadow-sm  text-center"> 
+                <img class="rounded-md" src="./assets/jilbab5.webp">
+                <h1> Nombre Producto </h1>
+                <p> 40 $</p>
+            </div>
+            <div class="border rounded-md p-3 basis-1/4 shadow-sm  text-center"> 
+                
+                <img class="rounded-md" src="./assets/jilbab6.webp">
+                <h1> Nombre Producto </h1>
+                <p> 40 $</p>
+            </div>
+            <div class="border rounded-md p-3 basis-1/4 shadow-sm  text-center"> 
+                
+                <img class="rounded-md" src="./assets/jilbab7.webp">
+                <h1> Nombre Producto </h1>
+                <p> 40 $</p>
+            </div>
+            <div class="border rounded-md p-3 basis-1/4 shadow-sm  text-center"> 
+                <img class="rounded-md" src="./assets/jilbab8.webp">
+                    <h1> Nombre Producto </h1>
+                      <p> 40$ </p> 
+                    
+            </div>
+        </div>
+    </div>
+    <div class="h-52">
+
+    </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header> 
+    <div class=" border-2 border-purple-800 flex justify-around items-center bg-gray-100 border-5 m-2  border-amber-300 rounded-md shadow-lg p-4 inline text-center h-18"> 
+        <div class="">
+            <img class="h-16 w-16" src="../assets/ramadan.png">
+        </div>
+        <div class="">
+            <ul class="inline flex">
+                <li class="mr-2 text-xl mx-3 cursor-pointer text-purple-600 hover:text-purple-800 hover:underline-offset-4 hover:underline"> Tienda</li>
+                <li class="mr-2 text-xl mx-3 cursor-pointer text-purple-600 hover:text-purple-800 hover:underline-offset-4 hover:underline"> Rebajas</li>
+                <li class="text-xl mx-3 cursor-pointer text-purple-600 hover:text-purple-800 hover:underline-offset-4 hover:underline"> Proyecto</li>
+            </ul>
+        </div>
+        <div>
+            <img class="h-12 w-12" src="../assets/carrito.png">
+        </div>
+    </div>
+    </header>
+</template>
