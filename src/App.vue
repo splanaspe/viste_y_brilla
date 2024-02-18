c148b0a6a0fa0fd9bf3342e6b80a1e767724ff27
Salvador Planas <101177251+splanaspe@users.noreply.github.com>
2024-02-18 18:56:30 +0100

    Gran trabajo hoy, he finalizado la configuracion del carrito: cuando se muestra y cuando desaparece y he hecho la vista interna del carrito, ahora falta configurar los metodos de sumar cantidad, quitar cantidad, etc

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,44 @@ import ProductoGaleria from './components/ProductoGaleria.vue';
 import {db} from './data/productos';
 
   const productos = ref([]);
+  const carrito = ref([]);
 
   onMounted( () => {
     productos.value=db
   })
-  console.log(productos)
+
+  const agregarProductoCarrito = (producto) => {
+    const existeCarrito = carrito.value.findIndex(productoC => productoC.id === producto.id)
+    if(existeCarrito >=0) {
+      carrito.value[existeCarrito].cantidad++
+      console.log("Producto ya en el carrito, sumamos +1"+producto.nombre)
+    }else{
+      producto.cantidad=1
+      carrito.value.push(producto)
+      console.log("Añadimos producto "+producto.nombre+" al carrito")
+    }  // accedemos con .value porque estamos en el script
+  }
 
 </script>
 
 <template>
-    <Header> </Header>
+    <Header
+    :carrito="carrito"> </Header>
     <div class="grid grid-cols-[15%_auto] text-start px-4 my-10 gap-6 relative">
         <div class="rounded-md self-start sticky top-4">
-            <ul class="py-4 px-5 text-center">
+            <ul class="py-4 px-5 text-center divide-y-4 divide-gray-100 divide-" role="list">
                 <li class="my-1 text-2xl cursor-pointer text-lg text-purple-800 hover:underline-offset-4 hover:underline ">  Jilbabs </li>
                 <li class="my-1 text-2xl cursor-pointer text-lg text-purple-800 hover:underline-offset-4 hover:underline "> Hijabs  </li>
                 <li class="my-1 text-2xl cursor-pointer text-lg text-purple-800 hover:underline-offset-4 hover:underline "> Abayas  </li>
                 <li class="my-1 text-2xl cursor-pointer text-lg text-purple-800 hover:underline-offset-4 hover:underline "> Bolsos  </li>
                 <li class="my-1 text-2xl cursor-pointer text-lg text-purple-800 hover:underline-offset-4 hover:underline ">  Accesorios </li>
             </ul>
-            
         </div>
         <div class="grid grid-cols-3 gap-6 content-start snap-y ">
         <ProductoGaleria
           v-for="producto in productos"
-          v-bind:producto="producto" />
+          v-bind:producto="producto"
+          @agregar-producto-carrito="agregarProductoCarrito"/>
       </div>
     </div>
     
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,44 @@
+<script setup>
+    import {ref} from 'vue'
+
+    const mostrarCarrito = ref(false);
+    let tiempo, tiempo2;
+    let haEntradoCarrito= ref(false);
+
+    const props = defineProps({
+        carrito:{
+            type: Array,
+            required: true
+        }
+    })
+
+    const ocultarCarritoHandler = () =>{
+        if(mostrarCarrito.value){
+            tiempo = setInterval( () =>{
+            mostrarCarrito.value=false;
+            haEntradoCarrito.value=false;
+        }, 500)
+        }       
+    }
+
+    const mostrarCarritoHandler = () =>{
+        clearInterval(tiempo)
+        clearInterval(tiempo2)
+        mostrarCarrito.value=true
+        haEntradoCarrito.value=false;
+
+        tiempo2 = setInterval( () =>{
+            if(!haEntradoCarrito.value){
+                mostrarCarrito.value=false;
+            }
+        }, 4000)
+    }
+</script>
+
 <template>
-    <header>
+    <header class="">
     
-        <div class="flex justify-around items-center  bg-purple-800 shadow-xl p-4 inline text-center h-18"> 
+        <div class="flex justify-around items-center  bg-purple-500 shadow-xl p-4 inline text-center h-18"> 
         <div class="">
             <img class="h-16 w-16 hover:cursor-pointer hover:scale-110 transition-transform duration-300" src="../assets/ramadan.png">
         </div>
@@ -12,11 +49,40 @@
                 <li class="text-2xl mx-3 cursor-pointer text-white hover:text-gray-100 hover:underline-offset-4 hover:underline"> Proyecto</li>
             </ul>
         </div>
-        <div>
+        <div @click="mostrarCarritoHandler">
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-cart4 hover:scale-125 transition-transform duration-200 hover:cursor-pointer" viewBox="0 0 16 16">
                 <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
             </svg>
         </div>
     </div>
+    <!-- CARRITO -->
+    <div v-if="mostrarCarrito" @mouseleave="ocultarCarritoHandler" @mouseenter="haEntradoCarrito = true" class="relative z-50">    
+    <div class="absolute rounded-md px-3 py-1 h-96 w-96 bg-purple-500 right-2 top-2">
+        <p v-if="carrito.length === 0" class="text-white text-xl"> No hay productos en el carrito </p>
+        <div v-else class="flex flex-col h-full">
+            <!-- Contenedor de los productos con desplazamiento -->
+            <div class="overflow-y-auto max-h-80 divide-y-2 bg-white rounded-md">
+                <div class="py-2" v-for="producto in carrito">
+                    <div class="flex flex-col items-center">
+                        <div class="flex items-center justify-center text-center">
+                            <p class="text-black font-bold mx-3"> {{ producto.nombre }} : </p>
+                            <span class="mx-1 px-1 text-2xl text-red-500  bg-white rounded-md cursor-pointer"> - </span>
+                            <span class="mx-1 text-xl"> {{ producto.cantidad }} </span>
+                            <span class="mx-1 px-1 text-xl text-green-500 bg-white rounded-md cursor-pointer"> + </span>
+                        </div>
+                    <div class="mt-2">
+                        <img class="rounded-md w-24 h-34 shadow-md" :src='"src/assets/"+producto.imagen'>
+                    </div>
+                </div>
+            </div>
+        </div >
+            
+            <!-- Botón Pagar, fuera del contenedor de desplazamiento -->
+            <button class="bg-white text-purple-800 p-2 rounded-md my-2 self-center mt-auto"> PAGAR </button>
+        </div>
+    </div>
+</div>
+
     </header>
+    
 </template>
